74d49d5451f1d375359393c029b425b237b9bcd1
adelbertclerge <adelbertclerge@gmail.com>
2021-04-25 21:15:29 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let buds = []
+    for (let i = 0; i < name.length; i++) {
+        buds.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    debugger
+    }
+    return buds;
+}
+
+
+function countDown() {
+    let count = 10
+    while (count >= 0) {
+        console.log(count--)
+    }
+}
